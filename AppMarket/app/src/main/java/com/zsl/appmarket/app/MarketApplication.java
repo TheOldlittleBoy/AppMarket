0e72dcc1c021bd620e522e8b98bf1b46aa85681e
TheOlelittleBoy <18672542272@163.com>
2019-09-02 11:55:11 +0800

    创建项目

---
+++ AppMarket/app/src/main/java/com/zsl/appmarket/app/MarketApplication.java
@@ -0,0 +1,16 @@
+package com.zsl.appmarket.app;
+
+import android.app.Application;
+
+/**
+ * @author zsl
+ * @date 2019/9/2.
+ */
+public class MarketApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+	}
+}
---
+++ AppMarket/app/src/main/java/com/zsl/appmarket/app/ui/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zsl.appmarket.app.ui.activity;
+
+import android.os.Bundle;
+
+import com.zsl.appmarket.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author zsl
+ * @date 2019/9/2.
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
